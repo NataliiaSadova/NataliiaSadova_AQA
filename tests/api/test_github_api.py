0f4fd0c71d78455b33171bb72a79983944102c88
Nataliia Sadova <natasha.popadyuk20@gmail.com>
2024-05-28 10:43:16 +0300

    Added new tests for api_search

--- tests/api/test_github_api.py
+++ tests/api/test_github_api.py
@@ -12,3 +12,10 @@ def test_user_exists(github_api):
 def test_user_not_exists(github_api):
     r = github_api.get_user('sadovanatasha')
     assert r['message'] == 'Not Found'
+
+@pytest.mark.api
+def test_repo_can_be_found(github_api):
+    r = github_api.search_repo('become-qa-auto')
+    assert r['total_count'] == 57   
+    assert 'become-qa-auto' in r['items'] [0] ['name']
+    
